17af703ccfbea3193791f238eb94e68e3c03870d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-11 14:45:08 -0600

    creating a home view to show a feature

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+const HomeView = () => {
+  return (
+    <View>
+      <Text>This is the home</Text>
+    </View>
+  );
+}
+
+export default HomeView;
