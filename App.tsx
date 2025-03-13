63678e9d2f0d26f778e96f6ad6cddf2d09b4e9dc
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-12 19:46:07 -0600

    cleaning app.tsx file

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import HomeView from './src/views/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <HomeView/>
-      <StatusBar style="auto" />
+      <HomeView />
     </View>
   );
 }
