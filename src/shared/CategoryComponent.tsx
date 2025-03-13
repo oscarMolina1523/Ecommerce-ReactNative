8f2244de95d6b4676c63d3f5dbf0ec6562ffdfdf
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-13 16:09:52 -0600

    adding a flatlist and gap between elements

--- src/shared/CategoryComponent.tsx
+++ src/shared/CategoryComponent.tsx
@@ -1,25 +1,38 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
+const categories = [
+  { id: '1', name: 'shirt' },
+  { id: '2', name: 'bag' },
+  { id: '3', name: 'glasses' },
+  { id: '4', name: 'glasses' },
+  { id: '5', name: 'glasses' },
+];
 const CategoryComponent = () => {
+  const flatListRef = useRef<FlatList>(null);
   return (
     <View style={styles.container}>
       <View style={styles.containerHeader}>
         <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Top Categories</Text>
         <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#F17547' }}>See all</Text>
       </View>
-      <View style={styles.containerData}>
-        <View style={styles.item}>
-          <Ionicons style={{color:'#919191'}} size={50} name="shirt" />
-        </View>
-        <View style={styles.item}>
-          <Ionicons style={{color:'#919191'}} size={50} name="bag" />
-        </View>
-        <View style={styles.item}>
-          <Ionicons style={{color:'#919191'}} size={50} name="glasses" />
-        </View>
-      </View>
+      <FlatList
+        style={styles.containerData}
+        ref={flatListRef}
+        data={categories}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Ionicons size={50} style={{color:'#919191'}} name={item.name} />
+          </View>
+        )}
+        // this is for add and gap between the elements
+        contentContainerStyle={{ gap: 4 }}
+      />
     </View>
   );
 };
@@ -29,7 +42,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: 'center',
     width: '90%',
-    gap:20
+    gap: 20
   },
   containerHeader: {
     width: '100%',
@@ -42,7 +55,6 @@ const styles = StyleSheet.create({
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
-    gap: 4
   },
   item: {
     display: 'flex',
@@ -51,9 +63,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderColor: '#D8D3D3',
     height: 100,
-    width:100,
+    width: 100,
     borderWidth: 2,
-    borderRadius:8,
+    borderRadius: 8,
   }
 });
 
