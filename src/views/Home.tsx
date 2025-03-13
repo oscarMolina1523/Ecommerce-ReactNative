3fb48fe4dea15ad434971c3899a3e0956f7d3b36
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-12 19:45:26 -0600

    testing desing in home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, } from "react-native";
 
 
 const HomeView = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This is the home</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default HomeView;
