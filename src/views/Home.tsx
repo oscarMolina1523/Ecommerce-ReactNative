315b1ec7ba35aca98c0855963a636657061eacf1
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-14 12:43:55 -0600

    implementing product preview logic in home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import CategoryComponent from "../shared/CategoryComponent";
 import NavbarComponent from "../shared/NavbarComponent";
+import ProductPreview from "../shared/ProductPreview";
 import PromotionsCarousel from "../shared/PromotionsCarousel";
 
 
 const HomeView = () => {
   return (
     <View style={styles.container}>
-      <NavbarComponent/>
-      <PromotionsCarousel/>
-      <CategoryComponent/>
+      <NavbarComponent />
+      <PromotionsCarousel />
+      <CategoryComponent />
+      <View style={styles.products}>
+        <ProductPreview />
+        <ProductPreview />
+      </View>
     </View>
   );
 }
@@ -18,14 +23,23 @@ const HomeView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'column',
+    flexDirection: 'column',
     width: '100%',
-    // backgroundColor: '#FF0000',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop:40,
-    gap:20,
+    paddingTop: 40,
+    gap: 20,
   },
+  products:{
+    display:'flex',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    alignItems:'flex-start',
+    alignContent:'flex-start',
+    width:'100%',
+    paddingHorizontal:20,
+    gap:4
+  }
 });
 
 export default HomeView;
