d1dd8a17e58f8e3ce92a791028947d14bb7c40ec
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-14 18:05:53 -0600

    implementing the logic to show all products

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import productsData from "../data/ProductsData";
 import CategoryComponent from "../shared/CategoryComponent";
 import NavbarComponent from "../shared/NavbarComponent";
 import ProductPreview from "../shared/ProductPreview";
@@ -8,37 +9,41 @@ import PromotionsCarousel from "../shared/PromotionsCarousel";
 
 const HomeView = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <NavbarComponent />
       <PromotionsCarousel />
       <CategoryComponent />
       <View style={styles.products}>
-        <ProductPreview />
-        <ProductPreview />
+        {productsData.map(product => (
+          <ProductPreview
+            product={product}
+          />
+        ))}
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     flexDirection: 'column',
     width: '100%',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
     paddingTop: 40,
+    paddingHorizontal: 10,
     gap: 20,
   },
-  products:{
-    display:'flex',
-    flexDirection:'row',
-    flexWrap:'wrap',
-    alignItems:'flex-start',
-    alignContent:'flex-start',
-    width:'100%',
-    paddingHorizontal:20,
-    gap:4
+  products: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    width: '100%',
+    // paddingHorizontal:20,
+    gap: 4
   }
 });
 
