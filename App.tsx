af1e3386ae1a563fac3862f462b624fc833e999c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-15 15:26:33 -0600

    implementing the provider of singleProductContext

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TouchableOpacity, View } from 'react-native';
 import HomeView from './src/app/Home';
 import ProductDetail from './src/app/ProductDetail';
+import { SingleProductProvider } from './src/context/SingleProductContext';
 
 // Definir los parámetros de cada pantalla
 export type RootStackParamList = {
@@ -15,38 +16,40 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        // screenOptions={{ //con esto podemos personalizar el navbar en si sus diseños
-        //   headerStyle: { backgroundColor: "#FEB53A" }, // Cambia el color del header
-        //   headerTintColor: "#fff", // Color del texto e íconos
-        //   headerTitleStyle: { fontWeight: "bold" }, // Estilos del título
-        // }}
-        screenOptions={{
-          headerTitleAlign: "center" // Centra el título 
-        }}
-        initialRouteName="Home">
-        <Stack.Screen options={{
-          title: "Home", // Cambia el título de la pantalla
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => alert("Menú abierto")}>
-              <Ionicons name="menu" size={28} color="black" />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <View style={{ flexDirection: "row", gap: 15, marginRight: 10 }}>
-              <TouchableOpacity onPress={() => alert("Buscar")}>
-                <Ionicons name="search" size={28} color="black" />
+    <SingleProductProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          // screenOptions={{ //con esto podemos personalizar el navbar en si sus diseños
+          //   headerStyle: { backgroundColor: "#FEB53A" }, // Cambia el color del header
+          //   headerTintColor: "#fff", // Color del texto e íconos
+          //   headerTitleStyle: { fontWeight: "bold" }, // Estilos del título
+          // }}
+          screenOptions={{
+            headerTitleAlign: "center" // Centra el título 
+          }}
+          initialRouteName="Home">
+          <Stack.Screen options={{
+            title: "Home", // Cambia el título de la pantalla
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => alert("Menú abierto")}>
+                <Ionicons name="menu" size={28} color="black" />
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => alert("Perfil")}>
-                <Ionicons name="person-circle-outline" size={28} color="black" />
-              </TouchableOpacity>
-            </View>
-          ),
-        }}
-        name="Home" component={HomeView} />
-        <Stack.Screen name="Details" component={ProductDetail} options={{ title: "Product Details" }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+            ),
+            headerRight: () => (
+              <View style={{ flexDirection: "row", gap: 15, marginRight: 10 }}>
+                {/* <TouchableOpacity onPress={() => alert("Buscar")}>
+                  <Ionicons name="search" size={28} color="black" />
+                </TouchableOpacity> */}
+                <TouchableOpacity onPress={() => alert("Profile")}>
+                  <Ionicons name="person-circle-outline" size={28} color="black" />
+                </TouchableOpacity>
+              </View>
+            ),
+          }}
+            name="Home" component={HomeView} />
+          <Stack.Screen name="Details" component={ProductDetail} options={{ title: "Product Details" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SingleProductProvider>
   );
 }
