84235ac0e15e31a1ddb6561ae4bf96256cbb006c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-17 14:49:32 -0600

    creating a context of user

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useState } from "react";
+import { CartItem } from "../models/Products";
+import { User } from "../models/User";
+import { UserContextType } from "./TypesContext";
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // User per deffect
+  const defaultUser : User = {
+    id: "1",
+    name: "John Doe",
+    userName: "johndoe",
+    email: "johndoe@example.com",
+    country: "USA",
+    city: "New York",
+    phoneNumber: "123-456-7890",
+    cart: [],
+  };
+
+  const [user, setUser ] = useState<User>(defaultUser);
+
+  const addToCart = (item: CartItem) => {
+    setUser((prevUser) => ({
+      ...prevUser ,
+      cart: [...prevUser.cart, item],
+    }));
+  };
+
+  const removeFromCart = (itemId: string) => {
+    setUser ((prevUser ) => ({
+      ...prevUser ,
+      cart: prevUser.cart.filter(item => item.id !== itemId),
+    }));
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser , addToCart, removeFromCart }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser Context must be used within a UserProvider");
+  }
+  return context;
+};
