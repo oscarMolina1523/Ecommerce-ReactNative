20cae07537b46cf9b2591c182bcf8137ca37a1c3
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-17 09:42:53 -0600

    updating profile of user and adding logout desing

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -6,20 +6,34 @@ const ProfileView = () => {
   return (
     <View style={styles.container}>
       <View style={[styles.containerChild, { backgroundColor: '#0601B4' }]} >
-        <View style={{ flexDirection: 'row', width: '70%', height: '100%', alignItems: 'center', justifyContent: 'space-between' , gap:4}}>
+        <View style={{ flexDirection: 'row', width: '70%', height: '100%', alignItems: 'center', justifyContent: 'space-between', gap: 4 }}>
           <View style={{ width: '40%', height: '80%', borderRadius: '50%', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
             <Image
               source={{ uri: 'https://i.pinimg.com/474x/48/6c/a0/486ca00640b169300b48e9ceacd8e401.jpg' }}
               style={{ objectFit: 'cover', width: '100%', height: '100%' }}
             />
           </View>
-          <View style={{ width: '60%', height: '100%', alignItems: 'flex-start', justifyContent: 'center', overflow: 'hidden' ,}}>
-            <Text style={{color:'#FFFFFF', fontWeight:'bold'}}>Itunuoluwa Abidoye</Text>
-            <Text style={{color:'#FFFFFF'}}>@Itunuoluwa</Text>
+          <View style={{ width: '60%', height: '100%', alignItems: 'flex-start', justifyContent: 'center', overflow: 'hidden', }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: 'bold' }}>Itunuoluwa Abidoye</Text>
+            <Text style={{ color: '#FFFFFF' }}>@Itunuoluwa</Text>
           </View>
         </View>
         <View style={{ flexDirection: 'row', width: '30%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
           <Ionicons size={30} style={{ color: '#FFFFFF' }} name='pencil' />
+        </View>
+      </View>
+      <View style={[styles.containerChild, { backgroundColor: '#D7D7D7' }]} >
+        <View style={{ flexDirection: 'row', width: '80%', height: '100%', alignItems: 'center', justifyContent: 'space-between', gap: 4 }}>
+          <View style={{ width: '20%', height: '50%', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', backgroundColor:'#F3F2FB', borderRadius:'50%' }}>
+            <Ionicons size={40} style={{ color: '#413DC6' }} name='log-out' />
+          </View>
+          <View style={{ width: '80%', height: '100%', alignItems: 'flex-start', justifyContent: 'center', overflow: 'hidden', }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 22, }}>Logout</Text>
+            <Text style={{ color: '#FFFFFF' }}>Further secure your account for safety</Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', width: '20%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          <Ionicons size={30} style={{ color: '#FFFFFF' }} name='chevron-forward' />
         </View>
       </View>
     </View>
@@ -33,7 +47,8 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+    gap: 6
   },
   containerChild: {
     width: '100%',
